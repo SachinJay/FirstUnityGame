9f4d25de320a6545c7d97716eb6c1681e5336423
Sachin Devasahayam <sjd232@cornell.edu>
2020-05-06 20:08:48 -0400

    Created script for player movement
    Right now it just turns off gravity
    Will do more later but have to get autocomplete working first

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    //Can instantiate anything like this
+    //Then, in unity, can drag the particular component you want to access into the corresopnding field
+    public Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //When you write to the log, it shows up when you play the game under the Console panel
+        //Debug.Log("Hello, World");
+
+        //Turns off gravity for the player cube
+        playerRb.useGravity = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
