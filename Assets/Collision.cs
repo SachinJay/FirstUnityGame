ced675bf96b22948f8b3935e25d1179def9c6a78
Sachin Devasahayam <sjd232@cornell.edu>
2020-05-07 21:31:20 -0400

    Added code to detect collisions
    -Added obstacle to collide with as well
    -When you collide, the player stops moving (disables the movement script)

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+
+    public PlayerMovement mvmt;
+
     void OnCollisionEnter(UnityEngine.Collision collision)
     {
-        Debug.Log("Ouchy you hit something");        
+        Debug.Log(collision.collider.name);
+        
+        if(collision.collider.tag == "Obstacle")
+        {
+            //When you collide with an obstacle, stop the player from moving
+            mvmt.enabled = false;
+
+            //IDEA: I saw an add explosion force, so I think maybe we should explode the player on contact that'd be neat
+        }
     }
 }
