7d8ccbf977b1a46cdb84a7d95e8e03ef8cd6618f
Sachin Devasahayam <sjd232@cornell.edu>
2020-05-07 21:15:39 -0400

    Made script for camera to follow player
    Could have made the player the parent of the camera object, however, this would result in the camera rotating if the player was knocked about which we don't want

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 camOffset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform refers to transform of the current game object, i.e. whatever this script is attached to
+        //in this case, a camera
+        transform.position = player.position+camOffset;
+    }
+}
