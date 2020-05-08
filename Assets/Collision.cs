be3c8c3a51aa322a28f2d113c3c356ca2e569400
Sachin Devasahayam <sjd232@cornell.edu>
2020-05-07 21:22:53 -0400

    Added code to detect collisions
    Added obstacle to collide with as well

---
+++ Assets/Collision.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        Debug.Log("Ouchy you hit something");        
+    }
+}
